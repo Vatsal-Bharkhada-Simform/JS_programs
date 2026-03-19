a556a04ac4ad71f188f4e3d1ace72593a1ee1063
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-03-19 12:08:15 +0530

    Refactor: Improve code for factorial generation.

--- problems_kaushal/3_generate_factorial.js
+++ problems_kaushal/3_generate_factorial.js
@@ -1,6 +1,14 @@
 function generateFactorial(num){
-    if(num <= 1) return (num < 0 ? 0n : BigInt(num));
-    return BigInt(BigInt(num) * generateFactorial(num-1));
+
+    // Recursive solution (Not recommended)
+    
+    // if(num <= 1) return (num < 0 ? 0n : BigInt(num));
+    // return BigInt(BigInt(num) * generateFactorial(num-1));
+
+    // Iterative solution
+    let ans = BigInt(1);
+    for(let i = 1n ; i <= BigInt(num) ; i++) ans *= i;
+    return ans;
 }
 
-console.log(generateFactorial(50));
+console.log(generateFactorial(5));
