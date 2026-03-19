133fe2bc6f4b98a56339ac0c0d0c7bcc4f8b38ba
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-03-19 16:59:17 +0530

    Refactor(6): Improve code to handle edge cases and add length check.

--- problems_kaushal/6_anagram_check.js
+++ problems_kaushal/6_anagram_check.js
@@ -1,5 +1,5 @@
 function checkAnagrams(str1, str2){
-    return (str1.toLowerCase().split('').sort().join('') === str2.toLowerCase().split('').sort().join(''));
+    return str1.trim().length === str2.trim().length && (str1.toLowerCase().split('').sort().join('') === str2.toLowerCase().split('').sort().join(''));
 }
 
-console.log(checkAnagrams("Hello", "leHlo"));
+console.log(checkAnagrams(" Hello", "leHlo"));
